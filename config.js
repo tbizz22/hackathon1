565d968ad3230d92904b7cf4961e18645f79eacd
Tyler Bisbee <Tyler.Bisbee@gmail.com>
2018-08-06 14:18:33 -0400

    updated menu functions

---
+++ config.js
@@ -0,0 +1,185 @@
+var o = {
+    "GlobalShortcuts": {
+        "Keys": [{
+                "Label": "Quick Search",
+                "key": "/",
+                "result": ""
+            },
+            {
+                "Label": "Change Quick Search Type - Person",
+                "key": "Alt+80",
+                "result": ""
+            },
+            {
+                "Label": "Change Quick Search Type - Job",
+                "key": "Alt+74",
+                "result": ""
+            },
+            {
+                "Label": "Change Quick Search Type - New Hire Category",
+                "key": "Alt+78",
+                "result": ""
+            },
+            {
+                "Label": "Change Quick Search Type - Talent Pools",
+                "key": "Alt+84",
+                "result": ""
+            },
+            {
+                "Label": "Change Quick Search Type - Company",
+                "key": "Alt+67",
+                "result": ""
+            },
+            {
+                "Label": "Change Quick Search Type - Room",
+                "key": "Alt+82",
+                "result": ""
+            },
+            {
+                "Label": "Change Quick Search Type - Event",
+                "key": "Alt+69",
+                "result": ""
+            },
+            {
+                "Label": "Go to Dashboard",
+                "key": "G,D",
+                "result": ""
+            },
+            {
+                "Label": "Compose Email",
+                "key": "G,E",
+                "result": ""
+            },
+            {
+                "Label": "Open Shortcut Help",
+                "key": "Shift+/",
+                "result": ""
+            }
+        ]
+    },
+    "Navigating": {
+        "Keys": [{
+                "Label": "In MiniBrowser - Next",
+                "key": "J",
+                "result": "recordRight_href"
+            },
+            {
+                "Label": "In MiniBrowser - Previous",
+                "key": "K",
+                "result": "recordLeft_href"
+            },
+            {
+                "Label": "Menu - Create",
+                "key": "M,1",
+                "result": function () {
+                    document.getElementById('Create_navdropdown').click();
+                    document.getElementById('Person_navdropdown').focus();
+                }
+            },
+            {
+                "Label": "Menu - Search",
+                "key": "M,2",
+                "result": function () {
+                    document.getElementById('Search_navdropdown').click();
+                    document.getElementById('Search_navdropdown').focus();
+                }
+            },
+            {
+                "Label": "Menu - Report",
+                "key": "M,3",
+                "result": function () {
+                    document.getElementById('Report_navdropdown').click();
+                    document.getElementById('Report_navdropdown').focus();
+                }
+            },
+            {
+                "Label": "Menu - Communicate",
+                "key": "M,4",
+                "result": function () {
+                    document.getElementById('Communicate_navdropdown').click();
+                    document.getElementById('Communicate_navdropdown').focus();
+                }
+            },
+            {
+                "Label": "Menu - Library",
+                "key": "M,5",
+                "result": function () {
+                    document.getElementById('Library_navdropdown').click();
+                    document.getElementById('Library_navdropdown').focus();
+                }
+            },
+            {
+                "Label": "Menu - Other",
+                "key": "M,6",
+                "result": function () {
+                    document.getElementById('Other_navdropdown').click();
+                    document.getElementById('Other_navdropdown').focus();
+                }
+            },
+            {
+                "Label": "Menu - Admin",
+                "key": "M,7",
+                "result": function () {
+                    document.getElementById('Admin_navdropdown').click();
+                    document.getElementById('Admin_navdropdown').focus();
+                }
+            }
+        ]
+    },
+    "Search Actions": {
+        "Keys": [{
+                "Label": "Search Action 1",
+                "key": "A,1",
+                "result": ""
+            },
+            {
+                "Label": "Search Action 2",
+                "key": "A,2",
+                "result": ""
+            },
+            {
+                "Label": "Search Action 3",
+                "key": "A,3",
+                "result": ""
+            },
+            {
+                "Label": "Search Action 4",
+                "key": "A,4",
+                "result": ""
+            },
+            {
+                "Label": "Search Action 5",
+                "key": "A,5",
+                "result": ""
+            },
+            {
+                "Label": "Search Action 6",
+                "key": "A,6",
+                "result": ""
+            }
+        ]
+    },
+    "Profile Actions": {
+        "Keys": [{
+                "Label": "Edit Profile",
+                "key": "E",
+                "result": ""
+            },
+            {
+                "Label": "Save",
+                "key": "Alt+83",
+                "result": ""
+            },
+            {
+                "Label": "Job Profile - Go to People Tab",
+                "key": "T,P",
+                "result": ""
+            },
+            {
+                "Label": "Jump to Field ",
+                "key": ",",
+                "result": ""
+            }
+        ]
+    }
+}
